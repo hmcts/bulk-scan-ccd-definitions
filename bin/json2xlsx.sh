63da6bfb9f65c9ded6599da7fca6760e3a3697d9
mdayican <mustafa.dayican@hmcts.net>
2019-09-02 11:42:48 +0100

    change the created xslx file name

--- bin/json2xlsx.sh
+++ bin/json2xlsx.sh
@@ -2,8 +2,17 @@
 
 DEFINITION_VERSION="dev"
 
+THEPATH=`pwd`
+ENVPATH=`dirname "$THEPATH"`
+
+CASETYPE=`basename "$THEPATH"`
+ENVFOLDER=`basename "$ENVPATH"`
+
+FILENAME="$ENVFOLDER-$CASETYPE-created-ccd"
+
 # Create the importer image with a version number
-docker build --no-cache -t hmctspublic.azurecr.io/sscs/ccd-definition-importer:$DEFINITION_VERSION -f ../../../docker/importer.Dockerfile .
+docker build --no-cache --build-arg FILE_NAME=$FILENAME -t hmctspublic.azurecr.io/sscs/ccd-definition-importer:$DEFINITION_VERSION -f ../../../docker/importer.Dockerfile .
+
 
 # Create the spreadsheet using the importer image created above
 docker run -ti --rm --name json2xlsx \
--- docker/importer.Dockerfile
+++ docker/importer.Dockerfile
@@ -7,4 +7,9 @@ RUN apk add --no-cache curl jq zip unzip git
 COPY --from=base . .
 COPY ./data /data
 
-CMD cd /opt/ccd-definition-processor && yarn json2xlsx -D /data/sheets -o /data/created-ccd.xlsx
+
+ARG FILE_NAME
+RUN echo "file name $FILE_NAME"
+ENV FILE_NAME ${FILE_NAME}
+
+CMD cd /opt/ccd-definition-processor && yarn json2xlsx -D /data/sheets -o /data/${FILE_NAME}.xlsx
