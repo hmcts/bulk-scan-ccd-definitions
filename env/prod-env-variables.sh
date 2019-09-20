0164063424e2d6a9e149e8777cc0cfd0a671c409
mdayican <mustafa.dayican@hmcts.net>
2019-09-20 11:58:09 +0100

    add prod env variables

---
+++ env/prod-env-variables.sh
@@ -0,0 +1,5 @@
+
+echo "apply PROD env variables..."
+
+export CCD_DEF_BULK_SCAN_ORCHESTRATOR_URL=http://bulk-scan-orchestrator-prod.service.core-compute-prod.internal
+export CCD_DEF_BULK_SCAN_SSCS_URL=http://sscs-bulk-scan-prod.service.core-compute-prod.internal
