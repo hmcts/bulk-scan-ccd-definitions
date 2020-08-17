16f648b02a8d851154ca907cd014fde347270624
mdayican <mustafa.dayican@hmcts.net>
2020-08-17 09:32:57 +0100

    remove sscs url

--- env/demo-env-variables.sh
+++ env/demo-env-variables.sh
@@ -2,4 +2,3 @@
 echo "apply DEMO env variables..."
 
 export CCD_DEF_BULK_SCAN_ORCHESTRATOR_URL=http://bulk-scan-orchestrator-demo.service.core-compute-demo.internal
-export CCD_DEF_BULK_SCAN_SSCS_URL=http://sscs-bulk-scan-demo.service.core-compute-demo.internal
