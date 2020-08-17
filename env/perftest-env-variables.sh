cf7bd4685faeac4e6410510f71f267b007ac0c20
mdayican <mustafa.dayican@hmcts.net>
2020-08-17 09:18:52 +0100

    add perftest vars

---
+++ env/perftest-env-variables.sh
@@ -0,0 +1,5 @@
+
+echo "apply PERFTEST env variables..."
+
+export CCD_DEF_BULK_SCAN_ORCHESTRATOR_URL=http://bulk-scan-orchestrator-perftest.service.core-compute-perftest.internal
+export CCD_DEF_BULK_SCAN_SSCS_URL=http://sscs-bulk-scan-perftest.service.core-compute-perftest.internal
